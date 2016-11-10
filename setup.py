c1a6a1c55229206077c76387552015b032ad0a90
Martim Nascimento <martim.nascimento@nexxera.com>
2016-11-10 09:49:08 -0200

    changing version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pytchuka",
-    version="0.0.3",
+    version="0.0.4",
     author="",
     author_email="",
     install_requires=["marshmallow", "falcon"],
